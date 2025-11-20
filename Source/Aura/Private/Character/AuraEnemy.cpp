2dc5beef53b13157ad63b1c9ce1c0f910566d1de
Morgan <m.garricks@lancaster.ac.uk>
2025-11-20 15:22:35 +0000

    highlight enemies on mouseover

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,24 @@
 
 #include "Character/AuraEnemy.h"
 
-void AAuraEnemy::HighlightActor()
+AAuraEnemy::AAuraEnemy()
 {
-
+    GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+    
 }
 
 void AAuraEnemy::HighlightActor()
 {
+    GetMesh()->SetRenderCustomDepth(true);
+    GetMesh()->SetCustomDepthStencilValue(250);
+    Weapon->SetRenderCustomDepth(true);
+    Weapon->SetCustomDepthStencilValue(250);
+
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+    GetMesh()->SetRenderCustomDepth(false);
+    Weapon->SetRenderCustomDepth(false);
 
 }
